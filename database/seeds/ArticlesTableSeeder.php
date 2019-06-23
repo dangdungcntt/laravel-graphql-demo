1c0c3d2a53c6a802fe8049569b119e52b38b049b
Dung Nguyen Dang <dangdungcntt@gmail.com>
2019-06-23 22:38:44 +0700

    First commit

---
+++ database/seeds/ArticlesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Article;
+use App\User;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Article::truncate();
+        Article::unguard();
+
+        $faker = Factory::create();
+
+        User::all()->each(function ($user) use ($faker) {
+            foreach (range(1, 5) as $i) {
+                Article::create([
+                    'user_id' => $user->id,
+                    'title'   => $faker->sentence,
+                    'content' => $faker->paragraphs(3, true),
+                ]);
+            }
+        });
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\User;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        $faker = Factory::create();
+        $password = bcrypt('secret');
+
+        User::create([
+            'name'     => $faker->name,
+            'email'    => 'graphql@test.com',
+            'password' => $password,
+        ]);
+
+        for ($i = 0; $i < 10; ++$i) {
+            User::create([
+                'name'     => $faker->name,
+                'email'    => $faker->email,
+                'password' => $password,
+            ]);
+        }
+    }
+}
